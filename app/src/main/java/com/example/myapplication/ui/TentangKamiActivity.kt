e71a2d6639bf52ed758eb678204b8afb966c3fd3
Ryunite <lazyryu12@gmail.com>
2025-04-24 14:02:10 +0700

    Update TentangKamiActivity.kt

--- app/src/main/java/com/example/myapplication/ui/TentangKamiActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/TentangKamiActivity.kt
@@ -1,4 +1,20 @@
 package com.example.myapplication.ui
 
-class TentangKamiActivity {
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class TentangKamiActivity : AppCompatActivity() {
+    private lateinit var btnBack: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tentang_kami)
+
+        btnBack = findViewById(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
 }
