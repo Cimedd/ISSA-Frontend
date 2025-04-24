254fe618d67bddb688b5df1ac01877137ac5e283
Ryunite <lazyryu12@gmail.com>
2025-04-24 14:02:06 +0700

    Update SplashActivity.kt

--- app/src/main/java/com/example/myapplication/ui/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/SplashActivity.kt
@@ -1,4 +1,38 @@
 package com.example.myapplication.ui
 
-class SplashActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+@Suppress("DEPRECATION")
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var sessionManager: SessionManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        sessionManager = SessionManager(this)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            if(sessionManager.isLogin() == true) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 3000)
+    }
 }
