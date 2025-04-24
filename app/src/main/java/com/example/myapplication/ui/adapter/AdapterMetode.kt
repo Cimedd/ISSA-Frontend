64b8f26438665e14e5a45f6fbda4dfa6671f754e
Ryunite <lazyryu12@gmail.com>
2025-04-24 14:03:07 +0700

    Update AdapterMetode.kt

--- app/src/main/java/com/example/myapplication/ui/adapter/AdapterMetode.kt
+++ app/src/main/java/com/example/myapplication/ui/adapter/AdapterMetode.kt
@@ -1,4 +1,58 @@
 package com.example.myapplication.ui.adapter
 
-class AdapterMetode {
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+import com.example.myapplication.ui.SessionManager
+import com.example.myapplication.dataclass.DataMetode
+import com.squareup.picasso.Picasso
+
+
+@SuppressLint("RecyclerView")
+
+class AdapterMetode(val context: Context, val metodeList: ArrayList<DataMetode>): RecyclerView.Adapter<AdapterMetode.MyViewHolder>() {
+    private lateinit var sessionManager : SessionManager
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.card_metode, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        sessionManager = SessionManager(context)
+        val currentItem = metodeList[position]
+
+        holder.txtName.text = currentItem.name
+
+        // on click
+        holder.itemView.setOnClickListener {
+            val code = currentItem.code
+            val name = currentItem.name
+            val icon = currentItem.icon
+
+            sessionManager.setMetode(code, name, icon)
+            (context as Activity).finish()
+        }
+
+        // load icon
+        Picasso.get()
+            .load(currentItem.icon)
+            .into(holder.icon)
+    }
+
+    override fun getItemCount(): Int {
+        return metodeList.size
+    }
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val txtName: TextView = itemView.findViewById(R.id.txtName)
+        val icon: ImageView = itemView.findViewById(R.id.icon)
+    }
 }
