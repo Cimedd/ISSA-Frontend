8e7147390023c3fc9e2e94397565dfe35f73f456
Ryunite <lazyryu12@gmail.com>
2025-04-24 13:57:46 +0700

    Update CatatanActivity

--- app/src/main/java/com/example/myapplication/ui/CatatanActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/CatatanActivity.kt
@@ -1,4 +1,12 @@
 package com.example.myapplication.ui
 
-class CatatanActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class CatatanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catatan)
+    }
 }
